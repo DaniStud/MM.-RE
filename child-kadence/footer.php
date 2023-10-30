c562a443338e921fa79924b219b946a78a32ba39
Daniel Wirenfeldt <90177568+DaniStud@users.noreply.github.com>
2023-10-30 04:17:27 +0100

    Add files via upload

---
+++ child-kadence/footer.php
@@ -0,0 +1,10 @@
+
+<footer>
+<a href="https://bananmaelk.dk/jarl/contact/" class="contactfoot">Contact</a>
+<a href="#" class="contentfoot">Content portfolio</a>
+	<a href="https://bananmaelk.dk/jarl/cookie-policy/" class="cookiesfoot">Cookies</a>
+	<a href="https://bananmaelk.dk/jarl/refund_returns/" class="refundingfoot">Refunding</a>
+	<a href="https://bananmaelk.dk/jarl/privacy-policy/" class="privacyfoot">Privacy</a>
+</footer>
+</body>
+</html>
